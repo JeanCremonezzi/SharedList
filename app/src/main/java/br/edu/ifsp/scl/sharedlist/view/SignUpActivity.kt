6c8b520e77564a5792bb6a6e312f6a5178dac3c1
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-06-18 10:41:17 -0300

    feat: add signup with email and password

--- app/src/main/java/br/edu/ifsp/scl/sharedlist/view/SignUpActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sharedlist/view/SignUpActivity.kt
@@ -2,8 +2,10 @@ package br.edu.ifsp.scl.sharedlist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.edu.ifsp.scl.sharedlist.R
 import br.edu.ifsp.scl.sharedlist.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
     private val binding: ActivitySignupBinding by lazy {
@@ -13,5 +15,22 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        with (binding) {
+            signUpBtn.setOnClickListener {
+                val email = emailEt.text.toString();
+                val password = emailEt.text.toString();
+
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(email, password)
+                    .addOnSuccessListener {
+                        Toast.makeText(this@SignUpActivity, "User registered!", Toast.LENGTH_LONG).show()
+                        finish()
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(this@SignUpActivity, "Failure while registering user!", Toast.LENGTH_LONG).show()
+                    }
+            }
+        }
     }
 }
