5b1905695247d1a0d958999a42acc9b9a8547ee5
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-06-18 11:35:17 -0300

    feat: add google sign in

---
+++ app/src/main/java/br/edu/ifsp/scl/sharedlist/view/BaseActivity.kt
@@ -0,0 +1,20 @@
+package br.edu.ifsp.scl.sharedlist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.scl.sharedlist.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+sealed class BaseActivity: AppCompatActivity() {
+    protected val googleSignInOptions: GoogleSignInOptions by lazy {
+        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+    }
+
+    protected val googleSignInClient: GoogleSignInClient by lazy {
+        GoogleSignIn.getClient(this, googleSignInOptions)
+    }
+}
--- app/src/main/java/br/edu/ifsp/scl/sharedlist/view/LoginActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sharedlist/view/LoginActivity.kt
@@ -1,14 +1,22 @@
 package br.edu.ifsp.scl.sharedlist.view
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import br.edu.ifsp.scl.sharedlist.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity() {
     private val binding: ActivityLoginBinding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
     }
+
+    private lateinit var arl: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +26,42 @@ class LoginActivity : AppCompatActivity() {
             signUpBtn.setOnClickListener {
                 startActivity(Intent(this@LoginActivity, SignUpActivity::class.java))
             }
+
+            googleLoginBtn.setOnClickListener {
+                val gsa: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this@LoginActivity)
+
+                if (gsa == null) {
+                    arl.launch(googleSignInClient.signInIntent)
+                } else {
+                    openMainActivity()
+                }
+            }
         }
+
+        arl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                val gsa: GoogleSignInAccount = task.result
+                val credential = GoogleAuthProvider.getCredential(gsa.idToken, null)
+
+                FirebaseAuth.getInstance()
+                    .signInWithCredential(credential)
+                    .addOnSuccessListener {
+                        Toast.makeText(
+                            this,
+                            "User authenticated!",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        openMainActivity()
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(this, "Failed to authenticate user!", Toast.LENGTH_LONG).show()
+                    }
+            }
+        }
+    }
+    private fun openMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
