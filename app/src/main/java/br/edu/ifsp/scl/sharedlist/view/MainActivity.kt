18c3586633543effdc166986612e7ea0b0fe753c
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-06-18 07:19:14 -0300

    chore: add view binding configuration

--- app/src/main/java/br/edu/ifsp/scl/sharedlist/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sharedlist/view/MainActivity.kt
@@ -3,10 +3,15 @@ package br.edu.ifsp.scl.sharedlist.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.edu.ifsp.scl.sharedlist.R
+import br.edu.ifsp.scl.sharedlist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
