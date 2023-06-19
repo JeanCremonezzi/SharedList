3771dd573204aab948d6e57758bf1bc7aab3fe79
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-06-19 00:09:16 -0300

    feat: add main activity action bar menu

--- app/src/main/java/br/edu/ifsp/scl/sharedlist/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sharedlist/view/MainActivity.kt
@@ -2,6 +2,7 @@ package br.edu.ifsp.scl.sharedlist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import br.edu.ifsp.scl.sharedlist.R
 import br.edu.ifsp.scl.sharedlist.databinding.ActivityMainBinding
 
@@ -13,5 +14,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
