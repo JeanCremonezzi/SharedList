78c02cdd7c0aa76ad5aeb06e48b53473320628f0
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-06-19 00:16:51 -0300

    feat: add sign out

--- app/src/main/java/br/edu/ifsp/scl/sharedlist/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sharedlist/view/MainActivity.kt
@@ -1,12 +1,15 @@
 package br.edu.ifsp.scl.sharedlist.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import br.edu.ifsp.scl.sharedlist.R
 import br.edu.ifsp.scl.sharedlist.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -19,5 +22,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.signOut -> {
+                FirebaseAuth.getInstance().signOut()
+                googleSignInClient.signOut()
+
+                startActivity(Intent(this, LoginActivity::class.java))
+                true
+            }
+
+            else -> false
+        }
     }
 }
