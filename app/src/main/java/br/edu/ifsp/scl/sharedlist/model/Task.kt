5e5667b68fa263bdf632d511cb9979b3c87c7865
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-06-19 07:00:29 -0300

    feat: Add Task model

--- app/src/main/java/br/edu/ifsp/scl/sharedlist/model/Task.kt
+++ app/src/main/java/br/edu/ifsp/scl/sharedlist/model/Task.kt
@@ -1,4 +1,20 @@
 package br.edu.ifsp.scl.sharedlist.model
 
+import android.os.Parcelable
+import androidx.annotation.NonNull
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Entity
 class Task (
-)
+    @PrimaryKey(autoGenerate = true) val id: Int? = -1,
+    @NonNull var title: String = "",
+    @NonNull var description: String = "",
+    @NonNull var createAt: String = "",
+    @NonNull var createBy: String = "",
+    @NonNull var estimatedFinishDate: String = "",
+    var finishedAt: String = "",
+    var finishedBy: String = "",
+): Parcelable
